9bc447961897b875efa9268627a455444b76ed83
萌大统领 <tom_kang@qq.com>
2020-08-31 20:11:33 +0800

    Create AddressDTO.java

---
+++ src/main/java/com/example/mybatisdemo/dto/AddressDTO.java
@@ -0,0 +1,24 @@
+package com.example.mybatisdemo.dto;
+
+public class AddressDTO {
+
+    private String address ;
+    private Long count;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+}
